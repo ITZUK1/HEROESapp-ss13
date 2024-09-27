e50eeccaaf95b86a467b6cd493df0f8ef6f1b064
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-09-27 10:23:09 -0500

    creacion de los diferentes enpoints SS15

--- HEROES-App/src/app/heroes/services/heroes.service.ts
+++ HEROES-App/src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { catchError, Observable, of } from "rxjs";
-import { Hero } from "../interfaces/hero.interface";
+import { catchError, map, Observable, of, pipe } from "rxjs";
+import { Hero } from '../interfaces/hero.interface';
 import { environments } from "src/environments/enviroment";
 
 @Injectable({providedIn:'root'})
@@ -11,25 +11,40 @@ export class HeroesService{
 
     constructor(private httpClient:HttpClient){}
 
+
+
+    //obtener todos los heroes
     getHeroes():Observable<Hero[]>{
         return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
     }
-
+    //obtener heroes por id
     getHeroByid(id:string): Observable<Hero | undefined> {
         return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`)
         .pipe(
             catchError(Error=>of(undefined))
         );
     }
-
+    //obtener sugerencias de heroes
     getSuggestions(query:string):Observable<Hero[]>{
         return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
     }
-
-
-
-
-
+    //agregar un nuevo heroe
+    addHero(hero:Hero):Observable<Hero>{
+        return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`,hero)
+    }
+    //actualizar un heroe existente
+    UpdateHero(hero:Hero):Observable<Hero>{
+        if(!hero.id)throw Error ('Hero id is required');
+        return this.httpClient.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`,hero)
+    }
+    //eliminar heroe por id 
+    DeleteHeroByid(id:string):Observable<boolean>{
+        return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`)
+        .pipe(
+            catchError(err=>of(false)),
+            map(resp=>true)
+        );
+    }
 
 }
 
