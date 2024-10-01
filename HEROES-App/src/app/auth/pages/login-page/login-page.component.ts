f9ff9c64b982b8514d46be96bfb9fdb4156f9472
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-01 08:39:16 -0500

    login funcionando

--- HEROES-App/src/app/auth/pages/login-page/login-page.component.ts
+++ HEROES-App/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,5 +10,25 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginPageComponent {
+
+  constructor ( 
+    private AuthService:AuthService,
+    private Router :Router
+  ){}
+
+  onLogin():void{
+
+    this.AuthService.login('mixato86@gmail.com','1234567')
+    .subscribe(User => {
+      this.Router.navigate(['/'])
+    });
+
+
+
+  }
+
+
+
+
 
 }
