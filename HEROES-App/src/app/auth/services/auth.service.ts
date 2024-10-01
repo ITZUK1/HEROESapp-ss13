25d462b82505af84f88490b8cafc1cafc61f069e
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-01 13:26:41 -0500

    se creo el metodo de autenticacion

--- HEROES-App/src/app/auth/services/auth.service.ts
+++ HEROES-App/src/app/auth/services/auth.service.ts
@@ -1,35 +1,54 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { environments } from "src/environments/enviroment"; // Corrige la ruta
-import { User } from '../interfaces/user.interface';
-import { Observable, tap } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({ providedIn: 'root' })
+import { Observable, tap, of, map, catchError } from 'rxjs';
+
+import { environments } from '../../../environments/enviroment';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({providedIn: 'root'})
 export class AuthService {
 
-    private baseUrl = environments.baseUrl;
-    private user?: User;
+  private baseUrl = environments.baseUrl;
+  private user?: User;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    get currentUser(): User | undefined {
-        if (!this.user) return undefined;
-        return structuredClone(this.user);
-    }
+  get currentUser():User|undefined {
+    if ( !this.user ) return undefined;
+    return structuredClone( this.user );
+  }
 
-    login(email: string, password: string): Observable<User> {
-        // http.post('login', { email, password });
+  login( email: string, password: string ):Observable<User> {
+    // http.post('login',{ email, password });
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k' )),
+      );
+  }
 
-        return this.http.get<User>(`${this.baseUrl}/users/1`)
-            .pipe(
-                tap(user => this.user = user),
-                tap(user => localStorage.setItem('token', 'LA.VIDA.ES.DURA1233K')),
-            );
-    }
+  checkAuthentication(): Observable<boolean> {
 
-    logout(){
-        this.user = undefined;
-        localStorage.clear();
-    }
+    if (!localStorage.getItem('token')) return  of (false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      );
+
+  }
+
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+
 
 }
